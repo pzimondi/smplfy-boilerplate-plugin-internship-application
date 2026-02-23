00673f4723082293ea6da5b6479ca64cf258981f
pzimondi <pzimondi@byupathway.edu>
2026-02-23 17:49:11 +0200

    css

--- public/js/frontend.js
+++
@@ -1,5 +0,0 @@
-// Simplify Custom Plugin JavaScript
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Simplify Custom Plugin loaded successfully!");
-    alert("The Boilerplate Plugin DEPLOYED to sandbox via GitHub Actions!");
-});
