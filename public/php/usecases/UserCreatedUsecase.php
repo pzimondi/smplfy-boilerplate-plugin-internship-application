c59e1a4e68523d8783db4b7aa2f2f4a27525c1b9
pzimondi <pzimondi@byupathway.edu>
2026-02-24 20:35:52 +0200

    memberpress-manager-transactions

--- public/php/usecases/UserCreatedUsecase.php
+++ public/php/usecases/UserCreatedUsecase.php
@@ -8,10 +8,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class UserCreatedUsecase {
 
+    /**
+     * Maps WordPress roles to MemberPress membership IDs.
+     * When a user is created manually in WP Admin with one of these roles,
+     * they will automatically get the corresponding membership transaction,
+     * which enables the MemberPress login redirect for their role.
+     * Add new roles here as the system grows.
+     */
     private array $role_to_membership = [
-        'manager' => FormIds::MANAGERS_MEMBERSHIP_ID,
+        'Manager' => FormIds::MANAGERS_MEMBERSHIP_ID,
     ];
 
+    /**
+     * Fired by WordpressAdapter via the user_register hook.
+     * Checks the new user's role and assigns the matching membership.
+     */
     public function handle_user_created( int $user_id ): void {
 
         $user = get_userdata( $user_id );
@@ -29,6 +40,7 @@ class UserCreatedUsecase {
 
                 MembershipTransactionUsecase::assign_membership_if_not_active( $user_id, $membership_id );
 
+                // Only assign the first matching role's membership
                 return;
             }
         }
