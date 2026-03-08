944aec14ed5a92e2f62183cf5c73aecdbff17ec1
pzimondi <pzimondi@byupathway.edu>
2026-03-08 16:07:42 +0200

    application form confirmation message

--- admin/DependencyFactory.php
+++ admin/DependencyFactory.php
@@ -4,7 +4,15 @@ namespace SMPLFY\boilerplate;
 
 class DependencyFactory {
 
+    private static bool $initialized = false;
+
     static function create_plugin_dependencies(): void {
+
+        // Prevent double instantiation
+        if ( self::$initialized ) {
+            return;
+        }
+        self::$initialized = true;
 
         $internshipApplicationUsecase = new InternshipApplicationUsecase();
         $userCreatedUsecase           = new UserCreatedUsecase();
