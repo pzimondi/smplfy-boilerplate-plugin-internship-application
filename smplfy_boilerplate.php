909b71688cd573338520f84800d3b31a31dd1706
pzimondi <pzimondi@byupathway.edu>
2026-04-05 16:03:00 +0200

    error loggin

--- smplfy_boilerplate.php
+++ smplfy_boilerplate.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * Plugin Name: SMPLFY Boiler Plate
  * Version: 1.0.0
@@ -21,17 +24,14 @@ define( 'SITE_URL', get_site_url() );
 define( 'SMPLFY_NAME_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SMPLFY_NAME_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-//Load files and run function that initialise the whole plugin
 require_once SMPLFY_NAME_PLUGIN_DIR . 'admin/utilities/smplfy_require_utilities.php';
 require_once SMPLFY_NAME_PLUGIN_DIR . 'includes/smplfy_bootstrap.php';
 
 bootstrap_boilerplate_plugin();
 
-//require_once SMPLFY_NAME_PLUGIN_DIR . 'includes/smplfy_bootstrap.php';
-
 function prevent_external_script_execution(): void {
-	if ( ! function_exists( 'get_option' ) ) {
-		header( 'HTTP/1.0 403 Forbidden' );
-		die;
-	}
+    if ( ! function_exists( 'get_option' ) ) {
+        header( 'HTTP/1.0 403 Forbidden' );
+        die;
+    }
 }
