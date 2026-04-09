3811a64049781822a4088ba2b11772004a745112
pzimondi <pzimondi@byupathway.edu>
2026-04-09 02:01:09 +0200

    Update debug-log-reader.php

--- debug-log-reader.php
+++ debug-log-reader.php
@@ -1,10 +1,9 @@
 <?php
-// Temporary debug file - delete after use
-$log = ABSPATH . 'wp-content/debug.log';
+$log = dirname( __FILE__ ) . '/debug-error.txt';
 if ( file_exists( $log ) ) {
     echo '<pre style="background:#fff;padding:20px;font-size:12px;">';
     echo htmlspecialchars( file_get_contents( $log ) );
     echo '</pre>';
 } else {
-    echo 'No debug.log found at: ' . $log;
+    echo 'No debug-error.txt found. Error may not have been caught yet.';
 }
