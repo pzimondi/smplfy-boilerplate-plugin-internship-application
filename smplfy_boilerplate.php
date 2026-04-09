14c7a2ed6e8ff86d491b927ceeedad741437d8a9
pzimondi <pzimondi@byupathway.edu>
2026-04-09 02:08:31 +0200

    Update smplfy_boilerplate.php

--- smplfy_boilerplate.php
+++ smplfy_boilerplate.php
@@ -1,6 +1,24 @@
 <?php
 namespace SMPLFY\boilerplate;
 
+// Override core's exception handler so we can see the error
+set_exception_handler( function( $e ) {
+    file_put_contents(
+        __DIR__ . '/debug-error.txt',
+        $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine() . "\n\n" . $e->getTraceAsString()
+    );
+} );
+
+register_shutdown_function( function() {
+    $error = error_get_last();
+    if ( $error && in_array( $error['type'], [ E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR ] ) ) {
+        file_put_contents(
+            __DIR__ . '/debug-error.txt',
+            print_r( $error, true )
+        );
+    }
+} );
+
 /**
  * Plugin Name: SMPLFY Boiler Plate Internship
  * Version: 1.0.0
@@ -24,16 +42,7 @@ define( 'SMPLFY_NAME_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once SMPLFY_NAME_PLUGIN_DIR . 'admin/utilities/smplfy_require_utilities.php';
 require_once SMPLFY_NAME_PLUGIN_DIR . 'includes/smplfy_bootstrap.php';
 
-add_action( 'plugins_loaded', function() {
-    try {
-        bootstrap_boilerplate_plugin();
-    } catch ( \Throwable $e ) {
-        file_put_contents(
-            SMPLFY_NAME_PLUGIN_DIR . 'debug-error.txt',
-            $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine() . "\n\n" . $e->getTraceAsString()
-        );
-    }
-} );
+add_action( 'plugins_loaded', 'SMPLFY\boilerplate\bootstrap_boilerplate_plugin' );
 
 function prevent_external_script_execution(): void {
     if ( ! function_exists( 'get_option' ) ) {
