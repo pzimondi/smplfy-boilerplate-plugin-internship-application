240cd26d496ad9ae6fbd2d0a5e3d2eda41ef24d6
pzimondi <pzimondi@byupathway.edu>
2026-04-17 07:54:57 +0200

    applicant inbox

--- public/php/usecases/WorkflowInbox.php
+++ public/php/usecases/WorkflowInbox.php
@@ -1,28 +1,2 @@
 <?php
-
-namespace SMPLFY\boilerplate;
-
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-class WorkflowInbox {
-
-    public function filter_inbox_entries( array $args ): array {
-
-        if ( ! is_user_logged_in() ) {
-            return $args;
-        }
-
-        $current_user  = wp_get_current_user();
-        $current_email = $current_user->user_email;
-
-        $args['field_filters'][] = [
-            'key'      => FormIds::INTERNSHIP_APPLICATION_EMAIL_FIELD_ID,
-            'value'    => $current_email,
-            'operator' => 'is',
-        ];
-
-        return $args;
-    }
-}
+// deprecated
