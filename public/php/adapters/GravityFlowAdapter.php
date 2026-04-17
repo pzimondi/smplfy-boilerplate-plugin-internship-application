41cfe4189e61abca7ed5ebedf77dd3b0f8cea05f
pzimondi <pzimondi@byupathway.edu>
2026-04-18 01:52:22 +0200

    applicant inbox

--- public/php/adapters/GravityFlowAdapter.php
+++ public/php/adapters/GravityFlowAdapter.php
@@ -12,11 +12,23 @@ class GravityFlowAdapter {
     }
 
     private function register_hooks(): void {
+
         add_action(
             'gravityflow_step_complete',
             [ $this->workflowNotifications, 'handle_step_complete' ],
             10,
             4
+        );
+
+        add_action(
+            'gravityflow_post_user_registration',
+            function( $user_id, $entry ) {
+                if ( $user_id ) {
+                    \GFAPI::update_entry_field( $entry['id'], '110', "user_id|{$user_id}" );
+                }
+            },
+            10,
+            2
         );
     }
 }
