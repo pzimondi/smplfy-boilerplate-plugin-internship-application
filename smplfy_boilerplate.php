cb212dd2271a6a8a22f131524cb2219830e41f6d
pzimondi <pzimondi@byupathway.edu>
2026-04-18 00:40:52 +0200

    logging

--- smplfy_boilerplate.php
+++ smplfy_boilerplate.php
@@ -4,6 +4,7 @@ namespace SMPLFY\boilerplate;
 ini_set( 'display_errors', 0 );
 ini_set( 'log_errors', 1 );
 ini_set( 'error_log', __DIR__ . '/debug-error.txt' );
+error_reporting( E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED );
 
 /**
  * Plugin Name: SMPLFY Boiler Plate Internship
