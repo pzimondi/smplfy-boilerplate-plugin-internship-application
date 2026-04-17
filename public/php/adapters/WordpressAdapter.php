957d3bf1d242c47077240630a7b5bc83aef3cf3f
pzimondi <pzimondi@byupathway.edu>
2026-04-17 12:58:18 +0200

    applicant inbox

--- public/php/adapters/WordpressAdapter.php
+++ public/php/adapters/WordpressAdapter.php
@@ -63,5 +63,14 @@ class WordpressAdapter {
                 $this->backfillMemberships->run();
             }
         );
+
+        add_action(
+            'gform_user_registered',
+            function( $user_id, $feed, $entry, $user_pass ) {
+                GFAPI::update_entry_property( $entry['id'], 'created_by', $user_id );
+            },
+            10,
+            4
+        );
     }
 }
